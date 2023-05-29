f4757b3b304bfb3353cda3ff41a6b07d808fa466
Undeadamien <126392901+Undeadamien@users.noreply.github.com>
2023-05-29 20:48:11 +0200

    implemented regex in fetch_srcs

--- pinterest_scraper.py
+++ pinterest_scraper.py
@@ -1,5 +1,6 @@
-from os import startfile, mkdir
-from os.path import exists, join, dirname
+import re
+from os import mkdir, startfile
+from os.path import dirname, exists, join
 from random import sample
 
 import requests
@@ -9,17 +10,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from undetected_chromedriver import Chrome
 
-
 OUTPUT_FOLDER: str = join(dirname(__file__), "output")
 EMAIL: str = ""
 PASSWORD: str = ""
 AMOUNT: int = 10
 
-if not exists(OUTPUT_FOLDER):
-    mkdir(join(dirname(__file__), "output"))
-
 
 def connect_to_google(driver: Chrome, wait: WebDriverWait):
+    """Log into Google account, wouldn't be possible without undetected_chromedriver"""
+
     driver.get("https://accounts.google.com/")
 
     # insert the email
@@ -38,33 +37,31 @@ def connect_to_google(driver: Chrome, wait: WebDriverWait):
 
 
 def connect_to_pinterest(driver: Chrome, wait: WebDriverWait):
+    """Log into Pinterest, to access user feed"""
+
     driver.get("https://www.pinterest.com")
 
     try:  # assure that we are connect by checking the profile picture
         xpath = "//*[@class='hCL kVc L4E MIw']"
         wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+
     except TimeoutError:  # reload to retry
         driver.refresh()
 
 
-def fetch_srcs(driver: Chrome, wait: WebDriverWait, amount):
+def fetch_srcs(driver: Chrome, wait: WebDriverWait, amount) -> list[str]:
+    """Fetch the largest jpg file from the srcs"""
+
     xpath = "//img[@srcset]"
     wait.until(EC.presence_of_all_elements_located((By.XPATH, xpath)))
     images = driver.find_elements(By.XPATH, xpath)
+
     srcs = []
 
-    # from the scrs we retrieve the src, to the largest jpg image
     for image in images:
-        links = [
-            link.strip()
-            for link in image.get_attribute("srcset").split()
-            if link.strip().endswith(".jpg")
-        ]
-        srcs.append(links[-1])
-
-    # reduce the number of images to return if the population is to small
-    if amount > len(srcs):
-        amount = len(srcs)
+        string = image.get_attribute("srcset")
+        jpgs = re.findall(r"https[^ ]+jpg", string)
+        srcs.append(jpgs[-1])
 
     return sample(srcs, amount)
 
@@ -74,17 +71,15 @@ def save_images(image_urls: list[str], destination_folder: str):
         name = url.split("/")[-1]
         path = f"{destination_folder}\\{name}"
 
-        if exists(path):
-            print(f"{name} already exist")
-            continue
-
         with open(path, "wb") as file:
             file.write(requests.get(url, timeout=10).content)
 
         print(f"{name} saved")
 
 
-def insert_search(driver: Chrome, wait: WebDriverWait, key_words: str):
+def insert_search(driver: Chrome, wait: WebDriverWait, key_words: str) -> None:
+    """Insert key words into the search bar"""
+
     xpath = "//input[@name='searchBoxInput']"
     wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
     search_box = driver.find_element(By.XPATH, xpath)
@@ -94,13 +89,17 @@ def insert_search(driver: Chrome, wait: WebDriverWait, key_words: str):
 
 def main():
     driver = Chrome()
-    wait = WebDriverWait(driver, 60)
     driver.maximize_window()
+    wait = WebDriverWait(driver, 60)
+
+    if not exists(OUTPUT_FOLDER):
+        mkdir(join(dirname(__file__), "output"))
 
     connect_to_google(driver, wait)
     connect_to_pinterest(driver, wait)
-    # insert_search(driver, wait, "photo reference")
-    save_images(fetch_srcs(driver, wait, AMOUNT), OUTPUT_FOLDER)
+    selected_src = fetch_srcs(driver, wait, AMOUNT)
+    save_images(selected_src, OUTPUT_FOLDER)
+
     startfile(OUTPUT_FOLDER)
 
 
